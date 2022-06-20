e6c87ad6d894e530c1485398bcc6d431fd7f3423
KimGeonWoo <ikkw2040@naver.com>
2022-06-20 21:30:47 +0900

    [ETC] 소수 / Easy/ 15분

---
+++ 소수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Main {
+
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n + 1];
+        for (int i = 2; i < n; i++) {
+            if (arr[i] == 0) {
+                answer++;
+            }
+            for (int j = 0; j < n; j = j + i) {
+                arr[j] = 1;
+            }
+        }
+
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(T.solution(n));
+    }
+}
