b035baa000c5750d59efb0ec67ec084c257920bc
KimGeonWoo <ikkw2040@naver.com>
2022-09-17 22:29:10 +0900

    [BeakJoon] DFS와 BFS프로그램 / Silver 2 / 20분

---
+++ DFS와 BFS 프로그램.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Main {
+    static boolean visited[];
+    static ArrayList<Integer>[] A;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int Start = sc.nextInt();
+
+        A = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            A[i] = new ArrayList<Integer>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            int S = sc.nextInt();
+            int E = sc.nextInt();
+
+            A[S].add(E);
+            A[E].add(S);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(A[i]);
+        }
+
+        visited = new boolean[N + 1];
+        DFS(Start);
+        System.out.println();
+        visited = new boolean[N + 1];
+        BFS(Start);
+        System.out.println();
+
+    
+    }
+
+    static void DFS(int node) {
+        System.out.print(node + " ");
+        visited[node] = true;
+        for (int i : A[node]) {
+            if (!visited[i]) {
+                DFS(i);
+            }
+        }
+    }
+
+    static void BFS(int Node) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(Node);
+        visited[Node] = true;
+
+        while (!queue.isEmpty()) {
+            int now_Node = queue.poll();
+            System.out.print(now_Node + " ");
+            for (int i : A[now_Node]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+}
