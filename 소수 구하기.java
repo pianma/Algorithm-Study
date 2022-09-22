1a01a6388308af6564522df8b975c9a62401a754
KimGeonWoo <ikkw2040@naver.com>
2022-09-22 23:38:31 +0900

    [BaekJoon] 소수구하기/ Silver2/ 20분

---
+++ 소수 구하기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] A = new int[N + 1];
+
+        for (int i = 2; i <= N; i++) {
+            A[i] = i;
+        }
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (A[i] == 0) {
+                continue;
+            }
+            for (int j = i + i; j < N; j = j + i) {
+                A[j] = 0;
+            }
+        }
+        for (int i = K; i <= N; i++) {
+            if (A[i] != 0) {
+                System.out.println(A[(int) i]);
+            }
+        }
+    }
+}
