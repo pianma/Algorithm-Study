86abe2e4803b06c438dfcb6dbc8ff71b7fbb2ea3
KimGeonWoo <ikkw2040@naver.com>
2022-10-05 01:56:44 +0900

    [BaekJoon] 팩토리얼/Bronze5/1분

---
+++ 팩토리얼.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int answer = 1;
+        for (int i = 1; i <= N; i++) {
+            answer = answer * i;
+        }
+
+        System.out.println(answer);
+    }
+}
