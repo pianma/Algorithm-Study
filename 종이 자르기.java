7d9a44df87b623fa313241ac33c21897a6b1ba8a
KimGeonWoo <ikkw2040@naver.com>
2023-03-08 23:43:28 +0900

    [PGS] 종이 자르기/ LV0/ 20분

---
+++ 종이 자르기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int M, int N) {
+        int answer = 0;
+        
+        int a = N -1 ;
+        int b = (M-1)*N;
+        
+        answer = a+b;
+        
+        return answer;
+    }
+}
