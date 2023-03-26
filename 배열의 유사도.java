81e51843cde5316ba12320bec9a80e600d8f99c8
KimGeonWoo <ikkw2040@naver.com>
2023-03-26 23:17:47 +0900

    [PGS] 배열의 유사도/ Lv0/ 15분

---
+++ 배열의 유사도.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        
+    for(String x : s1){
+            for(String y : s2){
+                if(x.equals(y)){
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
