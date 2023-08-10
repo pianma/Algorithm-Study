96f7be01a743aff485a8bd3b2cebefceb9b520af
KimGeonWoo <ikkw2040@naver.com>
2023-08-10 11:49:52 +0900

    경로탐색(Arraylist)

---
+++ 경로탐색(Arraylist).java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
+    static int n, m, answer = 0;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] ch;
+
+    public void DFS(int v) {
+        if (v == n) answer++;
+        else {
+            for (int x : graph.get(v)) {
+                if (ch[x]==0) {
+                    ch[x] = 1;
+                    DFS(x);
+                    ch[x] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        graph = new ArrayList<ArrayList<Integer>>();
+        ch = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+        }
+        ch[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
+    }
+}
